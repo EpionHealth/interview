388bc8dfdbf52aaa7e32659d24136aab09c2381e
gabriel coronel <gabriel.c1901@gmail.com>
2019-09-09 22:22:22 -0300

    Add form for check in

--- app/assets/javascripts/check_ins.js
+++ app/assets/javascripts/check_ins.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+window.addEventListener('load', function() {
+  document.getElementById('toggle_form').addEventListener('click', function () {
+    document.getElementById('check_in_form').style.display = 'inline';
+  });
+})
