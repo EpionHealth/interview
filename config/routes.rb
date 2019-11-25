ea66f1945c79c13ae861cce6acfec16f5b20f767
Jerred Cook <jerred.cook@gmail.com>
2019-11-25 09:44:56 -0500

    PHQ Screener files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  post   '/phq_screener/new',   to: 'phq_screener#create'
   resources :check_ins, only: [:new, :create, :show, :update]
-
+  resources :phq_screener, only: [:new]
   root to: "check_ins#new"
 end
