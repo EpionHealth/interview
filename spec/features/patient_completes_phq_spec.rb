157d1ff1b54eeb86f8c6b162ec0512b0b0c58604
Jason Platts <plattsjm@icloud.com>
2021-10-24 17:43:03 -0400

    Added depression screener result to rspec scenario.

--- spec/features/patient_completes_phq_spec.rb
+++ spec/features/patient_completes_phq_spec.rb
@@ -11,5 +11,21 @@ RSpec.feature "A patient checks into the app" do
     click_on "Start PHQ screener"
 
     expect(page).to have_content("Over the past 2 weeks, how often have you been bothered by any of the following problems?")
+
+    choose id: "response[0]_1"
+    choose id: "response[1]_3"
+
+    click_on "Continue"
+
+    expect(page).to have_content "The depression screener indicated you may have depression. Additional screening should be completed."
+
+    click_on "Start PHQ screener"
+
+    choose id: "response[0]_1"
+    choose id: "response[1]_1"
+
+    click_on "Continue"
+
+    expect(page).to have_content "The depression screener indicated you do not currently have signs and symptoms of depression."
   end
 end
