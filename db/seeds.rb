5d6b2502a768f1d3a119597b6a9c358349eb66f1
thecoderxs <ShehlaRiaz.us@gmail.com>
2024-07-10 10:46:24 +0500

    add seed file and update readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(title: "Little interest or pleasure in doing things?")
+Question.create(title: "Feeling down, depressed or hopeless?")
+FetchUserService.new.call
